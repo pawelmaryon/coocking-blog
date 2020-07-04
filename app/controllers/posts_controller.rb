4b5a14174c3348fe2a243289c6e9254da0b4bac2
pawelmaryon <maryonpawel@gmail.com>
2020-07-04 14:35:18 +0100

    cahnges to be reviewed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[post_params])
+
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
