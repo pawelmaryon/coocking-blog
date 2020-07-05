2b789e1669148502766070c9883b44c13c8ea586
pawelmaryon <maryonpawel@gmail.com>
2020-07-05 15:01:10 +0100

    post controller improvements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  
+
   def index
     @posts = Post.all
   end
@@ -13,23 +15,34 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to post_path(@post)
-    else
-      render new_post_path
-    end
+    @post.save
+    redirect_to post_path(@post)
+    
+  end
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
 
